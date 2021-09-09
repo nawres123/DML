3c4590c9368490df454e7add1f922e5230316f20
nawress <nawressmabrouk123@gmail.com>
2021-09-09 12:12:04 +0100

    first commit

---
+++ DML.sql
@@ -0,0 +1,37 @@
+INSERT INTO Product VALUES (
+	"P01",
+  	"Sumsung galaxy S20",
+  	"smart Phone",
+    1299
+);
+INSERT INTO Product VALUES (
+	"P02",
+  	"Asus notbook",
+  	"PC",
+    4599
+);
+
+INSERT INTO Customer VALUES (
+	"C01",
+  	"ALI",
+  	71321009
+);
+
+INSERT INTO Customer VALUES (
+	"C02",
+  	"ASMA",
+  	77345823
+);
+INSERT INTO Orders VALUES (
+  	"C01",
+  	"P02",
+    NULL,
+    2,
+  	9198
+);
+INSERT INTO Orders VALUES (
+  	"C02",
+  	"P01",
+    '28/5/2020',
+    1,
+  	3299
